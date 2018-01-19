5c03ae9872761e10aa99d2d2dd90c2db4292e142
piyush20432 <35602826+piyush20432@users.noreply.github.com>
2018-01-19 20:58:55 +0530

    created new Class to see upload operation in github.

---
+++ src/Scripts/TestUploadToGithub.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestUploadToGithub {
+
+	@Test
+	public void text() {
+		Reporter.log("Uploaded Successfully", true);
+		
+	}
+}
